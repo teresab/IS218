d3f4f39af8d5abe94d6a02ecbf19e27775caefb4
Teresa Bembury <tb94@njit.com>
2016-10-23 14:40:53 -0400

    added addtional code

--- readFile.php
+++ readFile.php
@@ -3,9 +3,18 @@ function readCSV($csvFile){
 	$file_handle= fopen($csvFile, 'r');
 	while (!feof ($file_handle)) {
 		$line_of_text[] = fgetcsv($file_handle, 1024);
-
 	
 	}
-
+fclose($file_handle);
+return $line_of_text;
 
 }
+
+//path to csv file
+$csvFile = 'profile.csv';
+
+$csv = readCSV(csvFile);
+echo '<pre>'
+print_r($csv);
+echo '</pre>';
+?>
