6cf019d8970d4c68e708b81ed466df4cc7bf021a
Teresa Bembury <tb94@njit.com>
2016-10-23 19:24:27 -0400

    added table tag

--- readFile.php
+++ readFile.php
@@ -17,4 +17,20 @@ $csv = readCSV(csvFile);
 echo '<pre>'
 print_r($csv);
 echo '</pre>';
+
+echo "<html><body><table> \n\n";
+$f = fopen($csvFile, 'r');
+while ( ($line =fgetcsv($f)) !=false) {
+
+	echo '<tr';
+	foreach ($line as $cell) {
+		echo '<td>' . htmlspecialchars($cell). '</td>';
+	
+	}
+	echo '</tr> \n';
+
+}
+fclose($f);
+echo '\n </table></body></html>";
 ?>
+
